9f8949026312b5cfcab1c091f12816a8243889ea
Jonathan Paugh <jpaugh@gmx.us>
2012-05-23 20:42:03 -0400

    Added debugging facility to gui.py
    For now, it's always on (or off). When I add add argparse support, it
    will be togglable via command line.

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@ import braille
 import wx
 import  wx.lib.filebrowsebutton as filebrowse
 
+debug = True
+
 class MyFrame(wx.Frame):
   def __init__(self, **kwargs):
     super(MyFrame, self).__init__(parent=None,
@@ -31,9 +33,27 @@ class InteractiveInput(wx.Panel):
     vSizer.Add(wx.StaticText(self, label='Braille output:'),
 	0, wx.ALIGN_LEFT|wx.ALL)
     vSizer.Add(self.outText, 1, wx.GROW|wx.NORTH)
+
+    if debug:
+      #Add a logging window
+      self.logger = OutputText(self)
+      self.logger.write = lambda s: self.logger.AppendText(s)
+      self.logger.resetText = lambda : self.logger.SetValue('')
+      braille.stderr = self.logger
+
+      #Add debug panel
+      self.dpanel = DebugPanel(self)
+
+      vSizer.Add(wx.StaticText(self, label='Log output:'),
+	  0, wx.ALIGN_LEFT|wx.ALL)
+      vSizer.Add(self.logger, 1, wx.GROW|wx.NORTH)
+      vSizer.Add(self.dpanel, 0)
+
     self.SetSizerAndFit(vSizer)
 
   def onTyping(self, event):
+    if debug:
+      self.logger.resetText()
     self.outText.SetValue(braille.convert(self.inText.GetValue()))
 
 class InputText(wx.TextCtrl):
@@ -47,6 +67,39 @@ class OutputText(wx.TextCtrl):
     defargs = {'style': wx.TE_MULTILINE|wx.TE_READONLY}
     defargs.update(kwargs)
     super(self.__class__, self).__init__(*args, **defargs)
+
+class DebugPanel(wx.Panel):
+  def __init__(self, *args, **kwargs):
+    super(self.__class__, self).__init__(*args, **kwargs)
+
+    shRuleset = wx.Button(self, wx.ID_ANY, label='Show &Ruleset')
+    self.Bind(wx.EVT_BUTTON, self.show_ruleset, shRuleset)
+
+    hSizer = wx.BoxSizer(wx.HORIZONTAL)
+    hSizer.Add(shRuleset, 1, wx.GROW)
+    self.SetSizerAndFit(hSizer)
+    return
+
+  def show_ruleset(self, event):
+    '''
+    Shows a listing of the current ruleset in another window.
+    '''
+    def format_ruleset(ruleset):
+      res = []
+      for i in xrange(len(ruleset)):
+	r=ruleset[i]
+	res.append('%d { prn: %s, brl %s, type: %s, pri: %d }' %
+	  (i, repr(r['prn']), repr(r['brl']), r['type'], r['priority']))
+      return '\n'.join(res)
+
+    diag = wx.Frame(parent=topLevel, size=(500,800),
+	title='Ruleset amer-2')
+    diag.text = OutputText(diag)
+    diag.text.SetValue(format_ruleset(braille.import_ruleset('amer-2')))
+    diag.vSizer = wx.BoxSizer(wx.VERTICAL)
+    diag.vSizer.Add(diag.text, 1, wx.GROW)
+    diag.SetSizerAndFit(diag.vSizer)
+    diag.Show()
 
 
 #TODO: Get this to work, too.
