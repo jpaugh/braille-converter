b26498f060e8eb499561640931da9bab51993e57
Jonathan Paugh <jpaugh@gmx.us>
2012-05-25 16:58:45 -0400

    Added regression tests for:
    - 145b9f642ec083bdf25b252
    - 0f427bb2294e6f773e872d6

--- braille/tests/test_regressions.py
+++ braille/tests/test_regressions.py
@@ -3,7 +3,10 @@ from . import BrTestCase, dots
 class Regressions(BrTestCase):
   def pairs(self):
     return (
+	'refined match for wcells: 145b9f642ec083bdf25b25268dfa1bc33a4dadc2',
 	('was.', dots('356 256')),
 	('but-this', dots('12 36 1456')),
+	'Abbrevs should be translated before prefixes 0f427bb2294e6f773e872d67d95b2c41153dfbe4',
+	('today', dots('2345 145')),
 	)
 
